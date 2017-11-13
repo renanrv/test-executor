f2cd386558c56255579eb5872b2da3c37326272d
Renan Vasconcelos <renan.vasconcelos@orama.com.br>
2017-11-13 12:22:02 -0200

    added validation method to check test requests data before saving it

--- testing/services/environment.py
+++ testing/services/environment.py
@@ -5,6 +5,12 @@ from testing.models.environment import Environment
 class EnvironmentService():
 
     @classmethod
+    def check_if_environment_is_available(cls, environment):
+        if environment.is_available:
+            return True
+        return False
+
+    @classmethod
     def get_environment_by_id(cls, environment_id):
         return Environment.objects.filter(id=environment_id).first()
 
@@ -15,3 +21,13 @@ class EnvironmentService():
     @classmethod
     def get_all_environments(cls):
         return Environment.objects.all()
+
+    @classmethod
+    def set_environment_available(cls, environment):
+        environment.is_available = True
+        environment.save(update_fields=["is_available"])
+
+    @classmethod
+    def set_environment_not_available(cls, environment):
+        environment.is_available = False
+        environment.save(update_fields=["is_available"])
--- testing/services/test_request.py
+++ testing/services/test_request.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from django.db import transaction
 from os import listdir
 from os.path import isfile, join
+from rest_framework.exceptions import ValidationError
 from testing.choices import TEST_REQUEST_STATUS_CHOICES, TEST_RUNNER_CHOICES
 from testing.constants import TEST_TEMPLATES_PATH, TEST_REQUEST_LOG_FILENAME, \
-    EXECUTION_TEST_TEMPLATES_PATH
+    EXECUTION_TEST_TEMPLATES_PATH, TEST_RUNNER_TYPE_PARAMETER, \
+    TEST_RUNNER_VERBOSITY_PARAMETER, TEST_RUNNER_DJANGO_MANAGE_PARAMETER, \
+    TEST_RUNNER_DJANGO_TEST_PARAMETER, TEST_RUNNER_NOSE_PARAMETER, \
+    TEST_RUNNER_PYTEST_PARAMETER, TEST_RUNNER_UNITTEST_PARAMETER
 from testing.models.test_request import TestRequest
 from testing.services.environment import EnvironmentService
 from testing.services.requester import RequesterService
@@ -18,12 +23,19 @@ import sys
 class TestRequestService():
 
     @classmethod
-    def check_if_test_request_log_displays_success(cls, test_request):
+    def _check_if_test_request_log_displays_success(cls, test_request):
         if (test_request.test_runner == cls._get_pytest_test_runner() and
                 "FAILURES" not in test_request.log) or \
                 (test_request.test_runner != cls._get_pytest_test_runner() and
                  "OK" in test_request.log):
             return True
+        return False
+
+    @classmethod
+    def _check_if_test_runner_is_valid(cls, test_runner):
+        for test_runner_option in TEST_RUNNER_CHOICES:
+            if test_runner_option[0] == test_runner:
+                return True
         return False
 
     @classmethod
@@ -34,7 +46,6 @@ class TestRequestService():
         test_runner = serialized_test_request.validated_data.get('test_runner')
         template = serialized_test_request.validated_data.get('template')
 
-        # TODO: check if environment is available
         test_request = cls._save_test_request(environment_id,
                                               requester_name,
                                               test_runner,
@@ -125,26 +136,26 @@ class TestRequestService():
         command = []
         if test_request.test_runner == cls._get_unittest_test_runner():
             command.append(sys.executable)
-            command.append("-m")
-            command.append("unittest")
-            command.append("-v")
+            command.append(TEST_RUNNER_TYPE_PARAMETER)
+            command.append(TEST_RUNNER_UNITTEST_PARAMETER)
+            command.append(TEST_RUNNER_VERBOSITY_PARAMETER)
             command.append(template)
         elif test_request.test_runner == cls._get_pytest_test_runner():
             command.append(sys.executable)
-            command.append("-m")
-            command.append("pytest")
-            command.append("-v")
+            command.append(TEST_RUNNER_TYPE_PARAMETER)
+            command.append(TEST_RUNNER_PYTEST_PARAMETER)
+            command.append(TEST_RUNNER_VERBOSITY_PARAMETER)
             command.append(template)
         elif test_request.test_runner == cls._get_nose_test_runner():
             command.append(sys.executable)
-            command.append("-m")
-            command.append("nose")
-            command.append("-v")
+            command.append(TEST_RUNNER_TYPE_PARAMETER)
+            command.append(TEST_RUNNER_NOSE_PARAMETER)
+            command.append(TEST_RUNNER_VERBOSITY_PARAMETER)
             command.append(template)
         elif test_request.test_runner == cls._get_django_testcase_test_runner():
             command.append(sys.executable)
-            command.append("manage.py")
-            command.append("test")
+            command.append(TEST_RUNNER_DJANGO_MANAGE_PARAMETER)
+            command.append(TEST_RUNNER_DJANGO_TEST_PARAMETER)
             command.append(template)
         return command
 
@@ -163,14 +174,19 @@ class TestRequestService():
                 log += reader.read().decode('utf-8')
             reader.read()
         test_request.log = log
-        if cls.check_if_test_request_log_displays_success(test_request):
+        if cls._check_if_test_request_log_displays_success(test_request):
             test_request.status = cls._get_suceeded_status()
         else:
             test_request.status = cls._get_failed_status()
-        test_request.save(update_fields=['status', 'log'])
+        with transaction.atomic():
+            test_request.save(update_fields=['status', 'log'])
+            EnvironmentService.set_environment_available(test_request.environment)
 
     @classmethod
     def _save_test_request(cls, environment_id, requester_name, test_runner, template, custom_path):
+        cls._validate_test_request_creation(environment_id, requester_name,
+                                            test_runner, template, custom_path)
+
         test_request = TestRequest()
         test_request.environment = \
             EnvironmentService.get_environment_by_id(environment_id)
@@ -182,5 +198,18 @@ class TestRequestService():
             cls._get_test_request_template_from_template_and_custom_path(template,
                                                                          custom_path)
         test_request.status = cls._get_requested_status()
-        test_request.save()
+        with transaction.atomic():
+            test_request.save()
+            EnvironmentService.set_environment_not_available(test_request.environment)
         return test_request
+
+    @classmethod
+    def _validate_test_request_creation(cls, environment_id, requester_name, test_runner, template, custom_path):
+        environment = \
+            EnvironmentService.get_environment_by_id(environment_id)
+        if environment is None:
+            raise ValidationError("Invalid environment.")
+        elif not EnvironmentService.check_if_environment_is_available(environment):
+            raise ValidationError("Environment is not available.")
+        elif not cls._check_if_test_runner_is_valid(test_runner):
+            raise ValidationError("Invalid test runner.")
